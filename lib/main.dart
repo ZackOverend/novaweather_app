9dce36aecb9f68e28894c9420c3875444ed4a292
ZackOverend <zack.overend@outlook.com>
2025-04-16 17:11:35 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const NovaWeatherApp());
+}
+
+class NovaWeatherApp extends StatelessWidget {
+  const NovaWeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NovaWeather',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: const HomeScreen(),
+    );
+  }
+}
