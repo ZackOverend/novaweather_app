18ec07e6560b2581a1a1882a80f2dd8d9136fe65
ZackOverend <zack.overend@outlook.com>
2025-04-16 18:32:42 -0400

    feat: added input field and listview

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,6 +13,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _controller = TextEditingController();
   final WeatherService _weatherService = WeatherService();
+  final LocationStorage _storage = LocationStorage();
+  List<SavedLocation> _locations = [];
+
   String _weatherInfo = "Enter a city to get the weather 🌍";
 
   void _getWeather() async {
@@ -33,50 +36,151 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _loadLocations();
+  }
+
+  Future<void> _loadLocations() async {
+    final loaded = await _storage.loadLocations();
+    setState(() {
+      _locations = loaded;
+    });
+  }
+
+  void _addLocation(String cityName) {
+    if (cityName.trim().isEmpty) return;
+
+    final newLocation = SavedLocation(
+      city: cityName.trim(),
+      country: "Unknown",
+    );
+
+    setState(() {
+      _locations.add(newLocation);
+      _controller.clear();
+    });
+
+    _storage.saveLocations(_locations);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("NovaWeather")),
+      appBar: AppBar(title: const Text("My Locations")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                labelText: 'City',
-                border: OutlineInputBorder(),
-              ),
+            Expanded(
+              child:
+                  _locations.isEmpty
+                      ? const Center(child: Text("No saved locations."))
+                      : ListView.builder(
+                        itemCount: _locations.length,
+                        itemBuilder: (context, index) {
+                          final loc = _locations[index];
+                          return Container(
+                            margin: const EdgeInsets.symmetric(vertical: 6),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.05),
+                                  blurRadius: 4,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: ListTile(
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 8,
+                              ),
+                              title: Text(
+                                loc.city,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              trailing: const Icon(
+                                Icons.arrow_forward_ios,
+                                size: 18,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
             ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _getWeather,
-              child: const Text("Get Weather"),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      hintText: "Enter a city...",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: () => _addLocation(_controller.text),
+                  child: const Text("Add"),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  final message = await _weatherService.pingServer();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text("Server says: $message")),
-                  );
-                } catch (e) {
-                  ScaffoldMessenger.of(
-                    context,
-                  ).showSnackBar(SnackBar(content: Text("Ping failed 🚫")));
-                }
-              },
-              child: const Text("Ping Server"),
-            ),
-
-            const SizedBox(height: 20),
-            Text(
-              _weatherInfo,
-              style: const TextStyle(fontSize: 18),
-              textAlign: TextAlign.center,
-            ),
+            SizedBox(height: 40),
           ],
         ),
       ),
     );
   }
 }
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(title: const Text("NovaWeather")),
+  //     body: Padding(
+  //       padding: const EdgeInsets.all(16.0),
+  //       child: Column(
+  //         children: [
+  //           TextField(
+  //             controller: _controller,
+  //             decoration: const InputDecoration(
+  //               labelText: 'City',
+  //               border: OutlineInputBorder(),
+  //             ),
+  //           ),
+  //           const SizedBox(height: 10),
+  //           ElevatedButton(
+  //             onPressed: _getWeather,
+  //             child: const Text("Get Weather"),
+  //           ),
+  //           ElevatedButton(
+  //             onPressed: () async {
+  //               try {
+  //                 final message = await _weatherService.pingServer();
+  //                 ScaffoldMessenger.of(context).showSnackBar(
+  //                   SnackBar(content: Text("Server says: $message")),
+  //                 );
+  //               } catch (e) {
+  //                 ScaffoldMessenger.of(
+  //                   context,
+  //                 ).showSnackBar(SnackBar(content: Text("Ping failed 🚫")));
+  //               }
+  //             },
+  //             child: const Text("Ping Server"),
+  //           ),
+
+  //           const SizedBox(height: 20),
+  //           Text(
+  //             _weatherInfo,
+  //             style: const TextStyle(fontSize: 18),
+  //             textAlign: TextAlign.center,
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
